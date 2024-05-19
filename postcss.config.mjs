a97be46daaf192fe7635d2794c6040b5023165e1
Hakan Okay <hokay.ca@gmail.com>
2024-05-19 11:56:00 -0400

    build(postcss.config.mjs): postcss config to use cssnano on production
    autoprefixer should you cssnano on production

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,11 @@
+// Please do not use the array form (like ['tailwindcss', 'postcss-preset-env'])
+// it will create an unexpected error: Invalid PostCSS Plugin found: [0]
+
 /** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
-
-export default config;

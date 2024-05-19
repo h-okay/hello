b997401f25c4876463c7470390fec98c532123bb
Hakan Okay <hokay.ca@gmail.com>
2024-05-19 12:02:59 -0400

    feat(Helpers.ts): added helpers
    added couple helper functions and variables

---
+++ src/utils/Helpers.ts
@@ -0,0 +1,22 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const IS_DEV = process.env.NODE_ENV === 'development';
+
+export const MILLISECONDS_IN_ONE_DAY = 86_400_000;
+
+export const getBaseUrl = () => {
+  if (process.env.NEXT_PUBLIC_APP_URL) {
+    return process.env.NEXT_PUBLIC_APP_URL;
+  }
+
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return 'http://localhost:3000';
+};

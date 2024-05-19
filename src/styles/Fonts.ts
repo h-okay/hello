337799840dddee626f21671a7f4071fd47b9ce45
Hakan Okay <hokay.ca@gmail.com>
2024-05-19 12:01:28 -0400

    feat(Fonts.ts): added new fonts
    added new fonts inter and poppins

---
+++ src/styles/Fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Poppins } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
